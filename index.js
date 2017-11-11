861b539b9c36d9fac4270d90adf00c7a6ead39e7
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-11 04:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name) {
-  const newDrivers = ['Broom', ...drivers];
+  const newDrivers = [drivers,...'Broom'];
   return newDrivers;
 }
